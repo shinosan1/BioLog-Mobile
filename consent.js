93f649ac6e1098920bfb6ba43b7be2a991689522
shinosan1 <shinoforge@gmail.com>
2026-07-14 21:05:06 +0900

    fix: 利用規約更新時の再同意とREADMEの整合性を修正

--- consent.js
+++ consent.js
@@ -3,7 +3,7 @@
 
   var STORAGE_KEY = "biolog_mobile_consent";
   var SCHEMA_VERSION = 1;
-  var TERMS_VERSION = "2026-07-14-2";
+  var TERMS_VERSION = "2026-07-14-3";
   var PRIVACY_VERSION = "2026-07-14-2";
   var ISO_TIMESTAMP_PATTERN = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
 
